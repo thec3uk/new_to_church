2941f250785969d3d89192862ebe539e660e550e
Andrew Miller <andrew.miller@founders4schools.org.uk>
2018-12-31 16:40:25 +0000

    switch to URL envvar

--- config/site.js
+++ config/site.js
@@ -4,8 +4,8 @@ module.exports = {
   titleAlt: 'The C3 Church', // Title for JSONLD
   description:
     'The C3 Church is an evangelical, charismatic Christian church based in Cambridge and Bury St Edmunds, England.',
-  url: process.env.DEPLOY_PRIME_URL || 'http://localhost:8000', // Domain of your site. No trailing slash!
-  siteUrl: `${process.env.DEPLOY_PRIME_URL}/` || 'http://localhost:8000/', // url + pathPrefix
+  url: process.env.URL || 'http://localhost:8000', // Domain of your site. No trailing slash!
+  siteUrl: `${process.env.URL}/` || 'http://localhost:8000/', // url + pathPrefix
   siteLanguage: 'en', // Language Tag on <html> element
   logo: 'static/logo/LogoWhite.png', // Used for SEO
   banner: 'static/logo/BackgroundGrey.png',
