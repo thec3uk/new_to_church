eff098afba52029a08ffe22476b44f29f7ee5202
Andrew Miller <andrew.miller@founders4schools.org.uk>
2019-02-10 12:20:42 +0000

    try a different name for email

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -22,7 +22,7 @@ class BaseForm extends React.Component {
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: encode({
         'form-name': this.props.formName,
-        'to-email': this.props.toEmail,
+        recipient: this.props.toEmail,
         ...this.props.data,
       }),
     })
