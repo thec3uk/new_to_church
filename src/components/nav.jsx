0576f26d6c2bc885a51e78be98edea8c014b7e4d
Andrew Miller <andrew.miller@founders4schools.org.uk>
2019-04-10 23:11:07 +0100

    restructure nav for mobile and notification bar instead of the slideshow

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import { StaticQuery, graphql, Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import Search from './search';
+import NotificationBar from 'react-notification-bar';
+import styles from './nav.module.css';
 
-const MenuItem = ({ menuItem }) => {
+const MenuItem = ({ menuItem, ...props }) => {
   const { title, slug } = menuItem;
   return (
-    <li className="menu_item menu_item_level_1">
-      <Link className="menu_link" to={slug}>
-        {title}
-      </Link>
+    <li className={styles.menu_item} {...props}>
+      {slug ? (
+        <Link className={styles.menu_link} to={slug}>
+          {title}
+        </Link>
+      ) : (
+        <span className={styles.menu_link}>{title}</span>
+      )}
     </li>
   );
 };
@@ -17,67 +23,200 @@ const MenuItem = ({ menuItem }) => {
 MenuItem.propTypes = {
   menuItem: PropTypes.shape({
     title: PropTypes.string.isRequired,
-    slug: PropTypes.string.isRequired,
+    slug: PropTypes.string,
   }),
 };
 
-const Nav = ({ index_page }) => {
-  var nav_class = '';
-  if (!index_page) {
-    nav_class = 'HeaderWhite LandingPage';
+class Nav extends React.Component {
+  constructor(props) {
+    super(props);
+    this.index_page = props.index_page;
+    var mobileNav = { open: false, splitIdx: this.splitTopNav() };
+    this.state = {
+      squashNav: false,
+      mobileNav: mobileNav,
+      nav_class: new Set(),
+    };
+    this._onScrollHandler = this._onScrollHandler.bind(this);
+    this.handleMoreClick = this.handleMoreClick.bind(this);
   }
-  return (
-    <StaticQuery
-      query={graphql`
-        query HeadingQuery {
-          site {
-            siteMetadata {
-              topNav {
-                title
-                slug
+
+  componentDidMount() {
+    requestAnimationFrame(this.squashNav);
+    window.addEventListener('scroll', this._onScrollHandler);
+    window.addEventListener('resize', this._onResizeHandler);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this._onScrollHandler);
+    window.removeEventListener('resize', this._onResizeHandler);
+  }
+
+  _onScrollHandler = () => {
+    this.setState({
+      squashNav: window.scrollY > 200,
+    });
+    requestAnimationFrame(this.squashNav);
+  };
+
+  _onResizeHandler = () => {
+    var splitIdx = this.splitTopNav();
+    this.setState(prevState => ({
+      mobileNav: {
+        ...prevState.mobileNav,
+        splitIdx: splitIdx,
+      },
+    }));
+  };
+
+  splitTopNav = () => {
+    if (window.innerWidth < 467) {
+      return 1;
+    } else if (window.innerWidth < 667) {
+      return 2;
+    } else if (window.innerWidth < 768) {
+      return 3;
+    } else if (window.innerWidth < 992) {
+      return 4;
+    }
+    return 5;
+  };
+
+  squashNav = () => {
+    var navClass = this.state.nav_class;
+    if (this.state.squashNav) {
+      navClass.add('HeaderSmall');
+      if (this.index_page) {
+        navClass.add(styles.HeaderWhite);
+      }
+    } else {
+      navClass = new Set();
+      if (!this.index_page) {
+        navClass = new Set([styles.HeaderWhite]);
+      }
+    }
+    this.setState({ nav_class: navClass });
+    requestAnimationFrame(this.squashNav);
+  };
+
+  handleMoreClick = () => {
+    this.setState(prevState => ({
+      mobileNav: {
+        ...prevState.mobileNav,
+        open: !prevState.mobileNav.open,
+      },
+    }));
+  };
+
+  render() {
+    return (
+      <StaticQuery
+        query={graphql`
+          query HeadingQuery {
+            site {
+              siteMetadata {
+                topNav {
+                  title
+                  slug
+                }
+              }
+            }
+            prismicNotificationBanner {
+              id
+              data {
+                content
+                active
+                url {
+                  link_type
+                  url
+                  target
+                }
               }
             }
           }
-        }
-      `}
-      render={data => {
-        const topNav = data.site.siteMetadata.topNav;
-        return (
-          <section
-            className={`slice_Header_T ActivateFixedPosition ${nav_class}`}
-          >
-            <div className="SlickNav_Wrapper">
-              <Search />
-            </div>
-            <div className="nav-menu">
-              <ul className="root_menu">
-                {topNav.map(menuItem => {
-                  return <MenuItem key={menuItem.slug} menuItem={menuItem} />;
-                })}
-              </ul>
-            </div>
-            <div className="logo-container">
-              <span className="logo-icon">
-                <Link to="/">
-                  <img
-                    alt=""
-                    className="LogoWhite"
-                    src="/Images/Content/4/Templates/49141/images/LogoWhite.png"
-                  />{' '}
-                  <img
-                    alt=""
-                    className="LogoGrey"
-                    src="/Images/Content/4/Templates/49141/images/LogoGrey.png"
-                  />{' '}
-                </Link>{' '}
-              </span>
-            </div>
-          </section>
-        );
-      }}
-    />
-  );
-};
+        `}
+        render={data => {
+          const nav = data.site.siteMetadata.topNav;
+          const notification = data.prismicNotificationBanner.data;
+          const topNav = nav.slice(0, this.state.mobileNav.splitIdx);
+          const verticalNav = nav.slice(
+            this.state.mobileNav.splitIdx,
+            nav.length
+          );
+          return (
+            <section
+              className={`slice_Header_T ActivateFixedPosition ${Array.from(
+                this.state.nav_class
+              ).join(' ')}`}
+              ref={c => (this.nav = c)}
+            >
+              {notification.active === 'yes' && (
+                <NotificationBar
+                  closeIconStyles={{
+                    width: 12,
+                    height: 12,
+                  }}
+                  sound={false}
+                  message={`<a href=${notification.url.url} target=${
+                    notification.url.target
+                  }>${notification.content}</a>`}
+                />
+              )}
+              <div className={styles.nav_menu}>
+                <div className="logo-container">
+                  <span className="logo-icon">
+                    <Link className={styles.logo_link} to="/">
+                      {this.state.nav_class.has(styles.HeaderWhite) ? (
+                        <img
+                          alt=""
+                          src="/Images/Content/4/Templates/49141/images/LogoGrey.png"
+                        />
+                      ) : (
+                        <img
+                          alt=""
+                          src="/Images/Content/4/Templates/49141/images/LogoWhite.png"
+                        />
+                      )}
+                    </Link>{' '}
+                  </span>
+                </div>
+                <ul className={styles.root_menu}>
+                  {topNav.map(menuItem => {
+                    return <MenuItem key={menuItem.slug} menuItem={menuItem} />;
+                  })}
+                  <MenuItem
+                    className={
+                      !this.state.mobileNav.open
+                        ? styles.more_menu_item
+                        : [styles.more_menu_item, styles.active].join(' ')
+                    }
+                    key={'more'}
+                    menuItem={{ title: 'More' }}
+                    onClick={this.handleMoreClick}
+                  />
+                </ul>
+
+                {this.state.mobileNav.open && (
+                  <ul className={styles.vertical_root_menu}>
+                    {verticalNav.map(menuItem => {
+                      return (
+                        <MenuItem
+                          className={styles.vertical_menu_item}
+                          key={menuItem.slug}
+                          menuItem={menuItem}
+                        />
+                      );
+                    })}
+                  </ul>
+                )}
+              </div>
+            </section>
+          );
+        }}
+      />
+    );
+  }
+}
 
 export default Nav;
 
