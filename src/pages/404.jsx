dc7bf48c21131b94d903cfd23568265c101a2f8f
Andrew Miller <andrew.miller@founders4schools.org.uk>
2019-04-12 13:23:51 +0100

    update 404 page to be slightly more informative

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -7,9 +7,17 @@ const ErrorPage = () => (
     <section className="slice_system_page">
       <div className="container system_content">
         <div className="systemPageContent">
-          <h1>Sorry</h1>
-          <p>The page you requested could not be found.</p>
-          <Link to="/">Go to the The C3 Church homepage.</Link>.
+          <h1>404. Page not found.</h1>
+          <p>
+            Sorry you cannot find the page you were looking for. We have
+            recently updated our website and unfortately some old URLs have
+            broken.
+          </p>
+          <Link to="/">Go to our homepage.</Link>.
+          <Link to="/online">Check out Church Online.</Link>.
+          <Link to="/bury">
+            Find out about our latest location in Bury St Edmunds.
+          </Link>
         </div>
       </div>
     </section>
