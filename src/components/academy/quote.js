efcb7be8d31983ccb4b963fa129906715437655a
Andrew Miller <info@akmiller.co.uk>
2020-04-30 13:43:33 +0100

    remove image from quote

--- src/components/academy/quote.js
+++ src/components/academy/quote.js
@@ -5,8 +5,8 @@ import HtmlHexagon from './HtmlHex';
 
 const Quote = ({ data }) => {
   return (
-    <section className={'px-8 lg:px-16 text-black lg:mx-32 mt-8 lg:mt-0 -mb-8 lg:-mb-32'}>
-      <div className="grid grid-cols-1 lg:grid-cols-2 grid-rows-2 row-gap-4 xl:row-gap-px xl:col-gap-48">
+    <section className={'px-8 lg:px-16 text-black lg:mx-32 mt-8 lg:mt-0 -mb-8 '}>
+      <div className="grid grid-cols-1 md:mx-20 lx:mx-48">
         <HtmlHexagon
           className="stroke-yellow fill-current text-purple"
           containerClassName="flex flex-col justify-between content-center"
@@ -16,17 +16,6 @@ const Quote = ({ data }) => {
             dangerouslySetInnerHTML={{ __html: data.primary.quote.html }}
           ></div>
         </HtmlHexagon>
-
-        <div className="row-start-2 row-end-3 lg:col-start-2 lg:col-end-3 font-title lowercase text-lg lg:-mt-56">
-          <Hexagon
-            style={{ stroke: 'inherit' }}
-            className="stroke-current text-purple"
-            backgroundImage={data.primary.portrait_author.url}
-            flatTop={true}
-            backgroundScale={1.05}
-            yOffset={10}
-          />
-        </div>
       </div>
     </section>
   );
