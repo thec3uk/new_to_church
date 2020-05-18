feffe4f233b73e2c1f47fec89eed6fc4b08ec2b9
Andrew Miller <info@akmiller.co.uk>
2020-05-18 15:47:14 +0100

    Update the 2Column Text to be more specific to StudyOptions
    Added a modal which opens on click to contain more information

--- src/components/academy/twoColumnText.js
+++ src/components/academy/twoColumnText.js
@@ -1,34 +1,106 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { graphql } from 'gatsby';
 import HtmlHexagon from './HtmlHex';
+import Transition from '../Transition'
+
+// className="bg-purple-trans text-white px-24 lg:px-32 py-32 lg:py-20 text-xl"
+
+const HexModal = ({ title, content, setModalOpen, colourClasses }) => (
+  <div
+    className={`${colourClasses} rounded px-4 pt-5 pb-4 mx-1 md:mx-8 max-h-full overflow-scroll shadow-xl transform transition-all max-w-full sm:w-full sm:p-6 `}
+    onClick={() => setModalOpen(false)}
+    role="dialog" aria-modal="true" aria-labelledby="modal-headline"
+  >
+    <div>
+      <div>
+        <svg className={`fill-current ${colourClasses} absolute right-0 mr-8 cursor-pointer`} xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" onClick={() => setModalOpen(false)}>
+          <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+        </svg>
+      </div>
+      <div dangerouslySetInnerHTML={{ __html: content }}></div>
+    </div>
+  </div>
+);
 
 const TwoColumnText = ({ data }) => {
+  const [colourClasses, setColourClasses] = useState("")
+  const [modalOpen, setModalOpen] = useState(false);
+  const [modalContent, setModalContent] = useState('');
+  const [modalTitle, setModalTitle] = useState('');
+
+  const openPTModal = () => {
+    setModalOpen(true);
+    setModalContent(data.primary.column_2.html);
+    setModalTitle('Part Time');
+    setColourClasses("bg-purple text-white")
+  };
+  const openFTModal = () => {
+    setModalOpen(true);
+    setModalContent(data.primary.column_1.html);
+    setModalTitle('Full Time');
+    setColourClasses("bg-yellow text-black")
+  };
   return (
     <section
       className={
         'px-8 lg:px-16 text-black grid grid-cols-1 grid-rows-3 lg:grid-rows-1 lg:grid-cols-8 row-gap-4 lg:row-gap-0 font-title lowercase mb-32 -mt-32 lg:mt-0'
       }
     >
+      {modalOpen && (
+        <div className="fixed bottom-0 inset-x-0 top-0 px-4 pb-4 pt-4 sm:inset-0 sm:p-0 sm:flex sm:items-center sm:justify-center">
+          <Transition
+            show={modalOpen}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 transition-opacity">
+              <div className="absolute inset-0 bg-gray-500 opacity-75" role="presentation" onClick={() => setModalOpen(false)}></div>
+            </div>
+          </Transition>
+          <Transition
+            show={modalOpen}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enterTo="opacity-100 translate-y-0 sm:scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          >
+            <HexModal
+              title={modalTitle}
+              content={modalContent}
+              colourClasses={colourClasses}
+              setModalOpen={setModalOpen}
+            />
+          </Transition>
+        </div>
+      )}
       <HtmlHexagon
         className="stroke-current text-yellow fill-current row-start-2 row-end-3 lg:row-start-1 lg:row-end-2 lg:col-start-1 lg:col-end-4"
         containerClassName="flex flex-col justify-between content-center"
+        onClick={() => openFTModal()}
       >
-        <div
-          className="text-black px-24 lg:px-32 py-20 text-xl"
-          dangerouslySetInnerHTML={{ __html: data.primary.column_1.html }}
-        ></div>
+        <div className="text-black py-48 text-center cursor-pointer">
+          <h3 className="text-5xl">Full Time</h3>
+          <p>Core leadership Training + Stream</p>
+        </div>
       </HtmlHexagon>
       <div className="text-black text-center -mx-8 lg:mx-auto mt-auto mb-8 lg:my-0 row-start-1 row-end-2 lg:col-start-4 lg:col-end-6">
         <h3 className="text-5xl">{data.primary.title}</h3>
       </div>
       <HtmlHexagon
         className="stroke-current text-purple fill-current row-start-3 row-end-4 lg:row-start-1 lg:row-end-2 lg:col-start-6 lg:col-end-9"
-        containerClassName="flex flex-col justify-between content-center"
+        containerClassName="flex flex-col justify-end h-full"
+        onClick={() => openPTModal()}
       >
-        <div
-          className=" text-white px-24 lg:px-32 py-32 lg:py-20 text-2xl"
-          dangerouslySetInnerHTML={{ __html: data.primary.column_2.html }}
-        ></div>
+        <div className="text-white text-center py-48 cursor-pointer">
+          <h3 className="text-5xl">Part Time</h3>
+          <p>Core leadership Training</p>
+        </div>
       </HtmlHexagon>
     </section>
   );
