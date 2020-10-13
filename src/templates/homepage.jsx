b013a203738b00039e7df31dc04c3c18296550d2
Andrew Miller <info@akmiller.co.uk>
2020-10-13 13:00:13 +0100

    Update homepage to reflect new service times

--- src/templates/homepage.jsx
+++ src/templates/homepage.jsx
@@ -46,14 +46,20 @@ const HomePage = ({ data }) => {
                   <hr />
                   <div className="links">
                     <h3>
+                      10:00:{` `}
                       <button href="#" onClick={clickHandler}>
-                        ONLINE 10:00
+                        ONLINE
                       </button>
+                      ,{` `}
+                      <Link to="/cambridge-sunday-services">
+                        Cambridge
+                      </Link>, <Link to="/bury-sunday-services">Bury</Link>
                     </h3>
-                    <h3 className="slash">/</h3>
+                    {/* <h3 className="slash">/</h3> */}
                     <h3>
-                      <Link to="/cambridge-sunday-services">Cambridge</Link> or{' '}
-                      <Link to="/bury-sunday-services">Bury</Link> @ noon
+                      Noon:{' '}
+                      <Link to="/cambridge-sunday-services">Cambridge</Link>
+                      {/* <Link to="/bury-sunday-services">Bury</Link> */}
                     </h3>
                     {/* <h3 className="slash">/</h3> */}
                     {/* <h3>
