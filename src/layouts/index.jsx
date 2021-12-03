cf335cf041d546ca24f5e4e9eeed7f8f354ec9de
Andrew Miller <info@akmiller.co.uk>
2021-12-03 15:54:48 +0000

    remove more old cruft

--- src/layouts/index.jsx
+++
@@ -1,4 +0,0 @@
-import Content from './Content';
-import { Layout, ContentLayout, HeroLayout } from './Layout';
-
-export { ContentLayout, HeroLayout, Content, Layout };
--- src/templates/index.jsx
+++
@@ -1,7 +0,0 @@
-import ContentPage from './content_page';
-import LandingPage from './landing_page';
-import TextPage from './text';
-import Slide from './slide';
-import HomePage from './homepage';
-
-export { ContentPage, LandingPage, TextPage, Slide, HomePage };
