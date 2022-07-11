06a03da49c8b36ea4cffca60921ba84b6c4272eb
Andrew Miller <info@akmiller.co.uk>
2022-07-11 14:40:52 +0100

    update text slice width

--- src/components/text/slice.tsx
+++ src/components/text/slice.tsx
@@ -4,7 +4,7 @@ const TextSlice = ({ children }) => {
   return (
     <div className="">
       <div className="px-0 mx-auto my-12 text-justify max-w-7xl sm:my-16 sm:px-3 lg:px-0">
-        <div className="max-w-3xl mx-auto prose lg:prose-xl prose-a:text-red-500 hover:prose-a:text-red-700">
+        <div className="max-w-3xl xl:max-w-screen-2xl mx-auto xl:mx-0 prose lg:prose-xl prose-a:text-red-500 hover:prose-a:text-red-700">
           {children}
         </div>
       </div>
