cd788777e05d1f6b817e03c9a01e3c90a93c6d75
Andrew Miller <info@akmiller.co.uk>
2022-08-10 15:29:31 +0100

    Add withVideo hero component

--- src/slices/header/index.tsx
+++ src/slices/header/index.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react'
+
 import TextHero from './textHero'
-
 import heroWithCustomComponent from './withComponent'
-
 import HeroWithImage from './withImage'
+import HeroWithVideo from './withVideo'
 
 const DefaultHeaderSlice = ({ slice }) => {
   return <div></div>
@@ -13,6 +13,7 @@ const HeaderSlice = ({ slice }) => {
   const variations: Record<string, React.ReactNode> = {
     default: DefaultHeaderSlice,
     heroWithImage: HeroWithImage,
+    heroWithVideo: HeroWithVideo,
     heroWithCustomComponent: heroWithCustomComponent,
     textOnlyHero: TextHero,
   }
