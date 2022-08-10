e77ff8e8bbf4557cc184224b4504386fafdc47b0
Andrew Miller <info@akmiller.co.uk>
2022-08-10 15:35:09 +0100

    update video media component

--- src/components/media/video.tsx
+++ src/components/media/video.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react'
+import { ResponsiveVideo } from '../responsiveVideo'
 
 import FullBleedMedia, { containerClassNames } from './fullbleed'
 
 const VideoMedia = ({ data }) => {
+  console.log(data)
+
   return (
     <FullBleedMedia>
-      <div
-        className={`${containerClassNames} prose`}
-        dangerouslySetInnerHTML={{ __html: data.html }}
-      />
+      <div className="w-screen h-full">
+        <ResponsiveVideo video={data.embed_url} playing volume={0} loop />
+      </div>
     </FullBleedMedia>
   )
 }
