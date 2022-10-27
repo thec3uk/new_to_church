f8a47cd65463cd3b400246b8f4c434e6f0c58ba8
Andrew Miller <info@akmiller.co.uk>
2022-10-27 11:23:35 +0100

    fix bg images

--- src/components/card/image.tsx
+++ src/components/card/image.tsx
@@ -34,7 +34,7 @@ const ImageCard = ({
       // Spread bgImage into BackgroundImage:
       {...bgImage}
       preserveStackingContext
-      className="h-56 bg-center bg-cover before:shadow before:rounded before:content-none md:h-72 lg:h-full lg:min-h-80"
+      className="h-56 bg-center bg-cover before:shadow before:rounded before:content-none md:h-72 lg:h-full aspect-video"
     >
       <div className={`${fromColour} bg-gradient-to-b`}>
         <TitleCard title={title} subtitle={subtitle} colour={colour} />
