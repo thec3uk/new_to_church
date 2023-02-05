539baa6b198db4be52274f49f7ab1bfaccb50c6c
Andrew Miller <info@akmiller.co.uk>
2023-02-05 10:18:31 +0000

    remove GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,29 +78,6 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: `UA-90682079-2`,
-        // Puts tracking script in the head instead of the body
-        head: true,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: [],
-        // Enables Google Optimize using your container Id
-        // optimizeId: 'YOUR_GOOGLE_OPTIMIZE_TRACKING_ID',
-        // Enables Google Optimize Experiment ID
-        // experimentId: 'YOUR_GOOGLE_EXPERIMENT_ID',
-        // Set Variation ID. 0 for original 1,2,3....
-        // variationId: 'YOUR_GOOGLE_OPTIMIZE_VARIATION_ID',
-        // Any additional create only fields (optional)
-        siteSpeedSampleRate: 10,
-        alwaysSendReferrer: true,
-      },
-    },
-    {
       resolve: `gatsby-plugin-mailchimp`,
       options: {
         endpoint:
